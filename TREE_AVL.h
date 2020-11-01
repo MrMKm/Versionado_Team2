917df534623a11eac40e2e8d0bae40377ffc13cf
Moises Hernandez Del toro <45748682+moiseshernandez26@users.noreply.github.com>
2020-11-01 17:20:41 -0600

    Issue #5 Añadir la clase Nodo para el arbol
    Se añade la clase nodo dentro de la clase árbol, falta la implementación de los métodos.

--- TREE_AVL.h
+++ TREE_AVL.h
@@ -12,6 +12,44 @@ class BTREE
 		BTREE();
 		BTREE(BTREE&);
 		~BTREE();
+		
+		class Node
+		{
+		public:
+			class NodeException : public std::exception {
+			private:
+				std::string msg;
+			public:
+				explicit NodeException(const char* message) : msg(message) {}
+
+				explicit NodeException(const std::string &message) : msg(message) { }
+
+				virtual ~NodeException() throw () {}
+
+				virtual const char* what() const throw () {
+					return msg.c_str();
+				}
+			};
+
+			Node();
+			Node(const T&);
+			~Node();
+
+			T& getData();
+			T*& getDataPtr();
+			Node*& getRight();
+			Node*& getLeft();
+
+			void setDataPtr(Node*&);
+			void setData(const T&);
+			void setRight(Node*&);
+			void setLeft(Node*&);
+
+		private:
+			T* dataPtr; //Apuntador al dato
+			Node* right;
+			Node* left;
+		};
 
 	private:
 		Node* root; //Raiz del Arbol
